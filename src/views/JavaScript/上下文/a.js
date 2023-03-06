e3f36cfb23311bd9f133a4c49f2ace6f7695f2ad
zhanglike <1760583856@qq.com>
2023-03-06 22:03:53 +0800

    手写call

---
+++ src/views/JavaScript/上下文/a.js
@@ -0,0 +1,15 @@
+let foo = function () {
+
+    console.log('foo1');
+
+}
+
+foo();  // foo1
+
+let foo = function () {
+
+    console.log('foo2');
+
+}
+
+foo(); // foo2
---
+++ src/views/JavaScript/手写call、bind、apply/call.js
@@ -0,0 +1,25 @@
+// 第一版
+Function.prototype.call2 = function (context) {
+    var context = context || window;
+    context.fn = this;
+    let arg = [...arguments].slice(1);
+    let result = context.fn(...arg);
+    delete context.fn;
+    return result;
+}
+
+// 测试一下
+// 测试一下
+var value = 2;
+
+var obj = {
+    value: 1
+}
+
+function bar() {
+    console.log(this.value);
+}
+
+bar.call2(null); // 2
+
+console.log(bar.call2(obj, 'kevin', 18));
