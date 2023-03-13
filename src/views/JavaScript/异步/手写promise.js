debd33183dd8b35697838d7dbfb9f8031ca88778
zhanglike <1760583856@qq.com>
2023-03-13 21:17:21 +0800

    基本的手写promise--v1-then方法随语雀

--- src/views/JavaScript/异步/手写promise.js
+++ src/views/JavaScript/异步/手写promise.js
@@ -1,6 +1,3 @@
-/**
- * 基本的手写promise
- */
 class MyPromise {
     // 构造方法
     constructor(executor) {
@@ -9,7 +6,6 @@ class MyPromise {
         this.initValue()
         // 初始化this指向
         this.initBind()
-
         try {
             // 执行传进来的函数
             executor(this.resolve, this.reject)
@@ -19,70 +15,65 @@ class MyPromise {
         }
     }
 
-    initValue() {
-        // 初始化值
-        this.PromiseResult = null // 终值
-        this.PromiseState = 'pending' // 状态
-        this.onFulfilledCallbacks = [];
-        this.onRejectedCallbacks = [];
-    }
-
     initBind() {
         // 初始化this
         this.resolve = this.resolve.bind(this)
         this.reject = this.reject.bind(this)
     }
 
+    initValue() {
+        // 初始化值
+        this.PromiseResult = null // 终值
+        this.PromiseState = 'pending' // 状态
+        this.onFulfilledCallbacks = [] // 保存成功回调
+        this.onRejectedCallbacks = [] // 保存失败回调
+    }
 
     resolve(value) {
-        if (this.PromiseState === 'pending') return;
+        // state是不可变的
+        if (this.PromiseState !== 'pending') return
         // 如果执行resolve，状态变为fulfilled
         this.PromiseState = 'fulfilled'
         // 终值为传进来的值
         this.PromiseResult = value
-        if (this.onFulfilledCallbacks.length) {
-            //获取第一个元素，并执行传参this.PromiseState
-            this.onFulfilledCallbacks.shift()(this.PromiseState)
+        // 执行保存的成功回调
+        while (this.onFulfilledCallbacks.length) {
+            this.onFulfilledCallbacks.shift()(this.PromiseResult)
         }
     }
 
     reject(reason) {
-        if (this.PromiseState === 'pending') return;
+        // state是不可变的
+        if (this.PromiseState !== 'pending') return
         // 如果执行reject，状态变为rejected
         this.PromiseState = 'rejected'
         // 终值为传进来的reason
-        this.PromiseResult = reason;
-        if (this.onRejectedCallbacks.length) {
-            this.onRejectedCallbacks.shift()(this.PromiseState)
+        this.PromiseResult = reason
+        // 执行保存的失败回调
+        while (this.onRejectedCallbacks.length) {
+            this.onRejectedCallbacks.shift()(this.PromiseResult)
         }
     }
 
     then(onFulfilled, onRejected) {
-        onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : val => val;
+        // 接收两个回调 onFulfilled, onRejected
+
+        // 参数校验，确保一定是函数
+        onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : val => val
         onRejected = typeof onRejected === 'function' ? onRejected : reason => {
             throw reason
-        };
+        }
+
         if (this.PromiseState === 'fulfilled') {
-            onFulfilled(this.PromiseState)
+            // 如果当前为成功状态，执行第一个回调
+            onFulfilled(this.PromiseResult)
         } else if (this.PromiseState === 'rejected') {
-            onRejected(this.PromiseState)
+            // 如果当前为失败状态，执行第二哥回调
+            onRejected(this.PromiseResult)
         } else if (this.PromiseState === 'pending') {
             // 如果状态为待定状态，暂时保存两个回调
             this.onFulfilledCallbacks.push(onFulfilled.bind(this))
             this.onRejectedCallbacks.push(onRejected.bind(this))
         }
-
-
     }
-
 }
-
-const test1 = new MyPromise((resolve, reject) => {
-    resolve('success')
-})
-console.log(test1) // MyPromise { PromiseState: 'fulfilled', PromiseResult: 'success' }
-
-const test2 = new MyPromise((resolve, reject) => {
-    reject('fail')
-})
-console.log(test2) // MyPromise { PromiseState: 'rejected', PromiseResult: 'fail' }
