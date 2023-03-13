f451389df84141778dab163c3313717e12531003
zhanglike <1760583856@qq.com>
2023-03-13 21:59:15 +0800

    基本的手写promise--v1-then方法支持链式调用

--- src/views/JavaScript/异步/手写promise.js
+++ src/views/JavaScript/异步/手写promise.js
@@ -1,3 +1,6 @@
+/**
+ * ddd
+ */
 class MyPromise {
     // 构造方法
     constructor(executor) {
@@ -57,23 +60,59 @@ class MyPromise {
 
     then(onFulfilled, onRejected) {
         // 接收两个回调 onFulfilled, onRejected
-
         // 参数校验，确保一定是函数
         onFulfilled = typeof onFulfilled === 'function' ? onFulfilled : val => val
         onRejected = typeof onRejected === 'function' ? onRejected : reason => {
             throw reason
         }
 
-        if (this.PromiseState === 'fulfilled') {
-            // 如果当前为成功状态，执行第一个回调
-            onFulfilled(this.PromiseResult)
-        } else if (this.PromiseState === 'rejected') {
-            // 如果当前为失败状态，执行第二哥回调
-            onRejected(this.PromiseResult)
-        } else if (this.PromiseState === 'pending') {
-            // 如果状态为待定状态，暂时保存两个回调
-            this.onFulfilledCallbacks.push(onFulfilled.bind(this))
-            this.onRejectedCallbacks.push(onRejected.bind(this))
-        }
+        const thenPromise = new MyPromise((resolve, reject) => {
+            const resolvePromise = cb => {
+                try {
+                    const x = cb(this.PromiseResult)
+                    if (x === thenPromise && x) {
+                        // 不能返回自身哦
+                        throw new Error('不能返回自身。。。')
+                    }
+                    if (x instanceof MyPromise) {
+                        // 如果返回值是Promise
+                        // 如果返回值是promise对象，返回值为成功，新promise就是成功
+                        // 如果返回值是promise对象，返回值为失败，新promise就是失败
+                        // 谁知道返回的promise是失败成功？只有then知道
+                        x.then(resolve, reject)
+                    } else {
+                        // 非Promise就直接成功
+                        resolve(x)
+                    }
+                } catch (err) {
+                    // 处理报错
+                    reject(err)
+                    throw new Error(err)
+                }
+            }
+
+            if (this.PromiseState === 'fulfilled') {
+                // 如果当前为成功状态，执行第一个回调
+                resolvePromise(onFulfilled)
+            } else if (this.PromiseState === 'rejected') {
+                // 如果当前为失败状态，执行第二个回调
+                resolvePromise(onRejected)
+            } else if (this.PromiseState === 'pending') {
+                // 如果状态为待定状态，暂时保存两个回调
+                // 如果状态为待定状态，暂时保存两个回调
+                this.onFulfilledCallbacks.push(resolvePromise.bind(this, onFulfilled))
+                this.onRejectedCallbacks.push(resolvePromise.bind(this, onRejected))
+            }
+        });
+
+        // 返回这个包装的Promise
+        return thenPromise
     }
 }
+
+
+let test2 = new MyPromise((resolve, reject) => {
+    setTimeout(() => {
+        resolve('success') // 1秒后输出 success
+    }, 1000)
+}).then(res => console.log(res), err => console.log(err))
