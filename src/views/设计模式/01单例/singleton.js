ff2c106c05a0bc7bf5f4846c730fd26f7a23a03c
zhanglike <1760583856@qq.com>
2023-03-23 08:20:47 +0800

    单例

--- src/views/设计模式/01单例/singleton.js
+++ src/views/设计模式/01单例/singleton.js
@@ -42,9 +42,61 @@ CreateSingleton.prototype.getName = function () {
     console.log(this.name);
 }
 
-let Winner = new CreateSingleton('Winner');
-let Looser = new CreateSingleton('Looser');
+// let Winner = new CreateSingleton('Winner');
+// let Looser = new CreateSingleton('Looser');
+//
+// console.log(Winner === Looser); // true
+// console.log(Winner.getName());  // 'Winner'
+// console.log(Looser.getName());  // 'Winner'
+/**
+ * 代理单例
+ */
+let ProxyCreateSingleton = (function () {
+    let instance;
+    return function (name) {
+        // 代理函数仅作管控单例
+        if (instance) {
+            return instance;
+        }
+        return instance = new Singleton(name);
+    }
+})();
+
+// 独立的Singleton类，处理对象实例
+let Singleton = function (name) {
+    this.name = name;
+}
+Singleton.prototype.getName = function () {
+    console.log(this.name);
+}
+
+let Winner = new ProxyCreateSingleton('Winner');
+let Looser = new ProxyCreateSingleton('Looser');
 
 console.log(Winner === Looser); // true
 console.log(Winner.getName());  // 'Winner'
 console.log(Looser.getName());  // 'Winner'
+
+/**
+ * 懒加载的单例
+ */
+let getSingleton = function (fn) {
+    var result;
+    return function () {
+        return result || (result = fn.apply(this, arguments)); // 确定this上下文并传递参数
+    }
+}
+let createAlertMessage = function (html) {
+    var div = document.createElement('div');
+    div.innerHTML = html;
+    div.style.display = 'none';
+    document.body.appendChild(div);
+    return div;
+}
+
+let createSingleAlertMessage = getSingleton(createAlertMessage);
+document.body.addEventListener('click', function () {
+    // 多次点击只会产生一个弹窗
+    let alertMessage = createSingleAlertMessage('您的知识需要付费充值！');
+    alertMessage.style.display = 'block';
+})
