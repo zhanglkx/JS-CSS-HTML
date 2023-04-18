b14d898f800694a374f5d3595fa14e56cca00b49
zhanglike <zhanglkx@foxmail.com>
2023-04-18 19:24:57 +0800

    🌟feat:(依赖) 👉重新引入loadash库

---
+++ src/views/loadash/loadashLearn.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>loadashLearn</h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import _ from "lodash";
+
+let array = ["1", 1];
+_.chunk(array, [size = 1]);
+</script>
+
+<style scoped>
+
+</style>
