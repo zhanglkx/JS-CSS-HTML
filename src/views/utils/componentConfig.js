97d6c5b567f853163effe091b92cb83dcad4ae25
zhanglike <zhanglkx@foxmail.com>
2023-04-24 09:17:54 +0800

    🔧chore:(目录更改) 👉构建过程或辅助工具的变动

--- src/views/utils/componentConfig.js
+++ src/views/utils/componentConfig.js
@@ -3,11 +3,11 @@ const componentConfig = [{
   nickName: "贪吃蛇",
   route: "/functionsList/Snake/subComponent"
 },
-  {
-    name: "farther2Son",
-    nickName: "父子相互传参",
-    route: "/functionsList/transmission/farther2Son"
-  }
+{
+  name: "farther2Son",
+  nickName: "父子相互传参",
+  route: "/functionsList/transmission/farther2Son"
+}
 ];
 
-export default  componentConfig;
+export default componentConfig;
