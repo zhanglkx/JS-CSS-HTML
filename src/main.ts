7f1e0cb34de78f8e22bbba78e22112d51c25fb9b
zhanglike <zhanglkx@foxmail.com>
2023-05-08 16:39:16 +0800

    🌟feat:(实现平台搭建) 👉错误捕获

--- src/main.ts
+++ src/main.ts
@@ -5,16 +5,17 @@ import store from "./store";
 
 import 'ant-design-vue/dist/antd.css';
 import Antd from 'ant-design-vue';
-import { ConfigProvider } from 'ant-design-vue';
-import 'ant-design-vue/dist/antd.variable.min.css';
 
 import './index.css'
 import './public/reset.css'
 
 const app = createApp(App);
+
 app.config.errorHandler = (err, vm, info) => {
-  console.log(err, vm, info)
+  console.log('错误对象：',err)
+  console.log('发生错误的组件实例：',vm)
+  console.log('包含错误来源信息的字符串：',info)
   // 处理错误
   // `info` 是 Vue 特定的错误信息，比如错误所在的生命周期钩子
 }
-app.use(store).use(router).use(ConfigProvider).use(Antd).mount("#app");
+app.use(store).use(router).use(Antd).mount("#app");
