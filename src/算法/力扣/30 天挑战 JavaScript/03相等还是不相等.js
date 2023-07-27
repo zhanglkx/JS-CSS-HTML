70cc781041ddbe0ca88531b1cd77bffd74ac183b
zhanglike <zhanglkx@foxmail.com>
2023-07-27 17:03:34 +0800

    feat:(算法)  30天挑战

--- src/算法/力扣/30 天挑战 JavaScript/03相等还是不相等.js
+++ src/算法/力扣/30 天挑战 JavaScript/03相等还是不相等.js
@@ -3,28 +3,44 @@
  * @return {Object}
  */
 const expect = function(val) {
-  return   {
-    toBe：function (value) {
-      if (val === value) {
+  return {
+    toBe: function(a) {
+      if (val === a) {
         return true;
       } else {
-        return "Not Equal";
+        throw new Error("Not Equal");
       }
-    }
+    },
 
-  notToBe：function (value) {
-      if (val !== value) {
+    notToBe: function(b) {
+      if (val !== b) {
         return true;
       } else {
-        return "Equal";
+        throw new Error("Equal");
       }
     }
-
   };
 };
-
+//一下方法执行速度赶快
+const expec1t = function(val) {
+  return {
+    toBe:function(a){
+      if(val===a)  return true
+      throw new  Error ("Not Equal")
+    },
+    notToBe:function(b){
+      if(val!==b){
+        return true
+      }else{
+        throw new Error("Equal")
+      }
+    }
+  };
+};
+//
 /**
  * expect(5).toBe(5); // true
  * expect(5).notToBe(5); // throws "Equal"
  */
-expect(5).; // true
+; // true
+console.log(expect(5).toBe(51));
