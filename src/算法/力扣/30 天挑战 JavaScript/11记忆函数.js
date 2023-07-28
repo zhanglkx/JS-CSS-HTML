c94339f252ddb51cf53fddfd1b995d27f259ddec
zhanglike <zhanglkx@foxmail.com>
2023-07-28 11:26:31 +0800

    feat:(算法)  30天挑战 -- 11记忆函数，不知为啥不通过

--- src/算法/力扣/30 天挑战 JavaScript/11记忆函数.js
+++ src/算法/力扣/30 天挑战 JavaScript/11记忆函数.js
@@ -2,19 +2,33 @@
  * @param {Function} fn
  */
 function memoize(fn) {
+  let maps = [];
+  let obj = { arg: [], result: 0 };
   return function(...args) {
-
-  }
+    let filterResult = maps.filter(word => word.arg.toString() === args.toString());
+    if (filterResult.length > 0)
+      return filterResult[0].result;
+    obj.arg = args;
+    obj.result = fn(...args);
+    maps.push(obj);
+    return obj.result;
+  };
 }
 
 
-/**
- * let callCount = 0;
- * const memoizedFn = memoize(function (a, b) {
- *	 callCount += 1;
- *   return a + b;
- * })
- * memoizedFn(2, 3) // 5
- * memoizedFn(2, 3) // 5
- * console.log(callCount) // 1
- */
+let callCount = 0;
+
+
+factorial = (n) => (n <= 1) ? 1 : (n * factorial(n - 1))
+
+let memoizedFn = memoize(factorial)
+
+
+// memoizedFn(2, 3); // 5
+// memoizedFn(2, 3); // 5
+console.log(memoizedFn(2));
+console.log(memoizedFn(3));
+console.log(memoizedFn(2));
+console.log(memoizedFn(3));
+console.log(callCount); // 1
+
