38a6935bcc92eb8666669108e9c9ac5af57072be
zhanglike <zhanglkx@foxmail.com>
2023-07-28 11:39:31 +0800

    feat:(算法)  30天挑战

--- src/算法/力扣/30 天挑战 JavaScript/11记忆函数.js
+++ src/算法/力扣/30 天挑战 JavaScript/11记忆函数.js
@@ -2,26 +2,35 @@
  * @param {Function} fn
  */
 function memoize(fn) {
-  let maps = [];
-  let obj = { arg: [], result: 0 };
+  let maps = new Map();
   return function(...args) {
-    let filterResult = maps.filter(word => word.arg.toString() === args.toString());
-    if (filterResult.length > 0)
-      return filterResult[0].result;
-    obj.arg = args;
-    obj.result = fn(...args);
-    maps.push(obj);
-    return obj.result;
+    if (!maps.has(args.toString())) {
+      maps.set(args.toString(), fn(...args));
+    }
+    return maps.get(args.toString());
   };
 }
+
+// 此方法更好
+function memoiz1e(fn) {
+  const map = new Map()
+  return function (...args) {
+    const item = args.join(',')
+    if (!map.has(item)) {
+      map.set(item, fn(...args))
+    }
+    return map.get(item)
+  }
+}
+
 
 
 let callCount = 0;
 
 
-factorial = (n) => (n <= 1) ? 1 : (n * factorial(n - 1))
+factorial = (n) => (n <= 1) ? 1 : (n * factorial(n - 1));
 
-let memoizedFn = memoize(factorial)
+let memoizedFn = memoize(factorial);
 
 
 // memoizedFn(2, 3); // 5
