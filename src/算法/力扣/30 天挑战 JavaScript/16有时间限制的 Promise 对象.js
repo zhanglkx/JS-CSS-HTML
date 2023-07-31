8ec2acacf2ec75e91da6ea6de2ebfab8cb102d99
zhanglike <zhanglkx@foxmail.com>
2023-07-31 09:07:57 +0800

    feat:(算法)  30天挑战-16-末模板

---
+++ src/算法/力扣/30 天挑战 JavaScript/16有时间限制的 Promise 对象.js
@@ -0,0 +1,14 @@
+/**
+ * @param {Function} fn
+ * @param {number} t
+ * @return {Function}
+ */
+const timeLimit = function(fn, t) {
+  return async function(...args) {
+
+  };
+};
+
+
+const limited = timeLimit((t) => new Promise(res => setTimeout(res, t)), 100);
+limited(150).catch(console.log); // "Time Limit Exceeded" at t=100ms
