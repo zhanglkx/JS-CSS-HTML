4518848a8a3c30adeca160cec2333326b01da3e6
zhanglike <zhanglkx@foxmail.com>
2023-08-03 11:38:23 +0800

    feat:(算法)  30天挑战-19-模板

--- src/算法/力扣/30 天挑战 JavaScript/18 函数防抖.js
+++ src/算法/力扣/30 天挑战 JavaScript/18 函数防抖.js
@@ -4,14 +4,18 @@
  * @return {Function}
  */
 const debounce = function(fn, t) {
+  let timer = null;
   return function(...args) {
-
+    if (timer) clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn(...args);
+    }, t);
   };
 };
 
-/**
- * const log = debounce(console.log, 100);
- * log('Hello'); // cancelled
- * log('Hello'); // cancelled
- * log('Hello'); // Logged at t=100ms
- */
+
+const log = debounce(console.log, 100);
+log("Hello"); // cancelled
+log("Hello"); // cancelled
+log("Hello"); // Logged at t=100ms
+
