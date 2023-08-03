c4d41bc637e41ae55246107572bc137b6fcea4b3
zhanglike <zhanglkx@foxmail.com>
2023-08-03 21:11:07 +0800

    feat: (js)  js挑战30天-19-优化代码

--- src/算法/力扣/30 天挑战 JavaScript/19并行执行异步函数.js
+++ src/算法/力扣/30 天挑战 JavaScript/19并行执行异步函数.js
@@ -5,8 +5,8 @@
 const promiseAll = async function(functions) {
   return new Promise((resolve, reject) => {
     const resArr = [];
-    let resCount = 0;
     functions.forEach((fn, i) => {
+      // promise函数也是函数，执行也是fn()，切记
       fn().then(res => {
         resArr[i] = res;
         if (i === functions.length-1) {
