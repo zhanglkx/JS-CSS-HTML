026c105fe9a74d7223c5abdbe1568289a125e493
zhanglike <zhanglkx@foxmail.com>
2023-08-03 17:59:35 +0800

    feat:(算法)  30天挑战-19-模板

--- src/算法/力扣/30 天挑战 JavaScript/19并行执行异步函数.js
+++ src/算法/力扣/30 天挑战 JavaScript/19并行执行异步函数.js
@@ -3,10 +3,21 @@
  * @return {Promise<any>}
  */
 const promiseAll = async function(functions) {
-
+  return new Promise((resolve, reject) => {
+    const resArr = [];
+    let resCount = 0;
+    functions.forEach((fn, i) => {
+      fn().then(res => {
+        resArr[i] = res;
+        if (i === functions.length-1) {
+          resolve(resArr);
+        }
+      }).catch(reject);
+    });
+  });
 };
 
-/**
- * const promise = promiseAll([() => new Promise(res => res(42))])
- * promise.then(console.log); // [42]
- */
+
+const promise = promiseAll([() => new Promise(res => res(42))]);
+promise.then(console.log); // [42]
+
