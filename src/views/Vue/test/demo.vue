899c6c0e8e48c782f619ace244cbe374481ee4a2
zhanglike <zhanglkx@foxmail.com>
2023-09-18 10:57:43 +0800

    🌟feat:(依赖) 👉更新依赖

---
+++ src/views/Vue/test/demo.vue
@@ -0,0 +1,45 @@
+<template>
+    <a-space>
+        <a-button type="primary" @click="() => open('topLeft')">
+            <RadiusUpleftOutlined />
+            topLeft
+        </a-button>
+        <a-button type="primary" @click="() => open('topRight')">
+            <RadiusUprightOutlined />
+            topRight
+        </a-button>
+    </a-space>
+    <a-divider />
+    <a-space>
+        <a-button type="primary" @click="() => open('bottomLeft')">
+            <RadiusBottomleftOutlined />
+            bottomLeft
+        </a-button>
+        <a-button type="primary" @click="() => open('bottomRight')">
+            <RadiusBottomrightOutlined />
+            bottomRight
+        </a-button>
+    </a-space>
+    <contextHolder />
+</template>
+<script lang="ts" setup>
+import {
+    RadiusBottomleftOutlined,
+    RadiusBottomrightOutlined,
+    RadiusUpleftOutlined,
+    RadiusUprightOutlined,
+} from '@ant-design/icons-vue';
+import { NotificationPlacement, notification } from 'ant-design-vue';
+const [api, contextHolder] = notification.useNotification();
+const open = (placement: NotificationPlacement) => openNotification(placement);
+const openNotification = (placement: NotificationPlacement) => {
+    api.info({
+        message: `Notification ${placement}`,
+        description:
+            'This is the content of the notification. This is the content of the notification. This is the content of the notification.',
+        placement,
+    });
+};
+</script>
+  
+  
