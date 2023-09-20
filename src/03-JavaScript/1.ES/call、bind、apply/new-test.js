b8585890fc42c409c1de609acd1b7da61901e444
zhanglike <zhanglkx@foxmail.com>
2023-09-20 20:15:00 +0800

    new
    new

--- src/03-JavaScript/1.ES/call、bind、apply/new-test.js
+++ src/03-JavaScript/1.ES/call、bind、apply/new-test.js
@@ -1,27 +1,32 @@
 // import {objectFactory} from "@/03-JavaScript/1.ES/call、bind、apply/new-1";
 const { objectFactory, objectFactory1, _new, newOperator } = require("./new-1");
 
-function Otaku(name, age) {
-  this.name = name;
-  this.age = age;
+// function Person(name, age) {
+//   this.name = name;
+//   this.age = age;
+//
+//   return 1;
+// }
 
-  this.habit = "Games";
+class Person {
+  constructor(name, age, habit) {
+    this.name = name;
+    this.age = 15;
+    this.habit = habit;
+  }
+
+  logInfo() {
+    console.log("我是日志！");
+  }
 }
 
-Otaku.prototype.strength = 60;
+// Person.prototype.habit = "Games";
+// Person.prototype.sayHi = function() {
+//   console.log("Hi " + this.name);
+// };
 
-Otaku.prototype.sayYourName = function() {
-  console.log("I am " + this.name);
-};
-
-
-// const person = objectFactory1(Otaku, "Kevin", "18");
-//
-// console.log(person.name); // Kevin
-// console.log(person.habit); // Games
-// console.log(person.strength); // 60
-//
-// person.sayYourName(); // I am Kevin
-
+let person = _new(Person, "后俊生", 18);
+console.log(person.name);   //后俊生
+console.log(person.age);    //18
 
 
