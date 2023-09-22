09155bf77a9fb5e94b08ffde087269b86de697e9
zhanglike <zhanglkx@foxmail.com>
2023-09-22 08:15:54 +0800

    🌟feat:(提交内容) 👉增加一下代码

--- src/03-JavaScript/进阶/debounce.js
+++ src/03-JavaScript/进阶/debounce.js
@@ -24,16 +24,14 @@ function debounce2(func, wait) {
     console.log(this)  //window
     return function () {
         var context = this;
-        console.log(this)  //<div id="container">1</div>
+        // console.log(this)  //<div id="container">1</div>
         clearTimeout(timeout)
-        timeout = setTimeout(function () {
-            func.apply(context)
-        }, wait);
+        timeout = setTimeout(function () { func.apply(context) }, wait);
     }
 }
 
 // 第三版 event 对象  如果我们不使用 debouce 函数，这里会打印 MouseEvent 对象，但是在我们实现的 debounce 函数中，却只会打印 undefined!
-function debounce(func, wait) {
+function debounce3(func, wait) {
     var timeout;
 
     return function () {
@@ -41,9 +39,41 @@ function debounce(func, wait) {
         var args = arguments;
 
         clearTimeout(timeout)
-        timeout = setTimeout(function(){
-            func.apply(context, args)
-        }, wait);
+        timeout = setTimeout(function () { func.apply(context, args) }, wait);
+    }
+}
+
+// V4立即执行
+/*
+ * 这个时候，代码已经很是完善了，但是为了让这个函数更加完善，我们接下来思考一个新的需求。这个需求就是：
+我不希望非要等到事件停止触发后才执行，我希望立刻执行函数，然后等到停止触发 n 秒后，才可以重新触发执行。
+想想这个需求也是很有道理的嘛，那我们加个 immediate 参数判断是否是立刻执行。
+ * @param {any} func 
+ * @param {any} wait 
+ * @param {any} immediate 
+ * @returns 
+* */
+function debounce(func, wait, immediate) {
+    var timeout;
+
+    return function () {
+        var context = this;
+        var args = arguments;
+
+        if (timeout) clearTimeout(timeout);
+
+        if (immediate) {
+            // 如果已经执行过，不再执行
+            var callNow = !timeout;
+            timeout = setTimeout(function () { timeout = null; }, wait)
+
+            if (callNow) func.apply(context, args)
+
+        } else {
+            timeout = setTimeout(function () {
+                func.apply(context, args)
+            }, wait);
+        }
     }
 }
 
