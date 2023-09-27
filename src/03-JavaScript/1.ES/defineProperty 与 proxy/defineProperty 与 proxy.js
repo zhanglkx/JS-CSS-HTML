f7904105609f079fad486a71f69d49166532a101
zhanglike <zhanglkx@foxmail.com>
2023-09-27 21:36:58 +0800

    Update defineProperty 与 proxy.js
    数据劫持

--- src/03-JavaScript/1.ES/defineProperty 与 proxy/defineProperty 与 proxy.js
+++ src/03-JavaScript/1.ES/defineProperty 与 proxy/defineProperty 与 proxy.js
@@ -1,9 +1,27 @@
-var obj = {};
+const obj = {}, value = null;
+
+
+// Object.defineProperty(obj, "num", {
+//     value : 1,
+//     writable : true,
+//     enumerable : true,
+//     configurable : true
+// });
+
+// console.log('1',obj);
+
+
 Object.defineProperty(obj, "num", {
-    value : 1,
-    writable : true,
-    enumerable : true,
-    configurable : true
+
+  get: function() {
+    console.log("execute get");
+    return value;
+  },
+  set: function(newValue) {
+    console.log("execute set");
+    value = newValue;
+  }
 });
 
-console.log('1',obj);
+obj.num = 1;
+console.log(obj.num);
